52dd3b8842081839beb036b80f3d9a996261917e
soyeon5-5 <ems5-5@naver.com>
2023-05-31 16:50:13 +0900

    modify s3 bucket name

--- aws/ap-northeast-2/terraform-backend/terraform-backend.tf
+++ aws/ap-northeast-2/terraform-backend/terraform-backend.tf
@@ -10,7 +10,7 @@ resource "aws_s3_bucket" "test-s3-tf-state0505" {
 
 resource "aws_dynamodb_table" "test-ddb-tf-lock" {
 
-  depends_on   = [aws_s3_bucket.test-s3-tf-state]
+  depends_on   = [aws_s3_bucket.test-s3-tf-state0505]
   name         = "test-ddb-tf-lock"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
